168aaeac797c75fb1516177fc16f532a51650c6d
abdulazizbumar270-eng <abdulazizbumar270@gmail.com>
2025-12-10 02:20:25 -0800

    Delete chatsystemapp directory

--- chatsystemapp/chatapp/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .import consumers
-
-websocket_urlpatterns = [
-    path('ws/chat/<int:conversation_id>/', consumers.ChatConsumer.as_asgi()),
-    path('ws/notifications/', consumers.NotificationsConsumer.as_asgi()),
-]
